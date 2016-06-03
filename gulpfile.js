fff57f4b37fc904834eee64845477a0c03846d9f
HeJason-lyon <616026889@qq.com>
2016-06-03 11:48:30 +0800

    第一次Pc

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint');
+
+
+gulp.task('webServe', function () {
+    connect.server({
+        root: 'www/',
+        livereload: true
+    });
+});
+
+/**压缩js */
+gulp.task('miniJs',function(){
+    return gulp.src('./www/js/**/*.js').pipe(uglify()).pipe(gulp.dest('./www/mini'));
+});
+/**压缩CSS */
+gulp.task('miniCss',function(){
+    return gulp.src('./www/css/**/*.css').pipe(minifyCss()).pipe(gulp.dest('./www/mini'));
+});
+
+// /**合并文件 */
+// gulp.task('concatJs',function () {
+//     return gulp.src('./www/js/**.js').pipe(concat()).pipe(gulp.dest('./www/'));
+// })
+// gulp.task('concatCss',function () {
+//     return gulp.src
+// })
+
+/** Sass编译 */
+gulp.task('sass',function() {
+    return gulp.src('./www/sass/**/*.scss').pipe(sass().on('error',sass.logError)).pipe(gulp.dest('./www/css'))
+});
+/** 自动刷新 */
+gulp.task('html', function () {
+    gulp.src('./www/**/*.html').pipe(connect.reload());
+})
+gulp.task('js', function () {
+    gulp.src('./www/js/**/*.js').pipe(connect.reload());
+})
+gulp.task('css', function () {
+    gulp.src('./www/css/**/*.css').pipe(connect.reload());
+})
+
+/**监视任务 */
+gulp.task('watch', function () {
+    gulp.watch(['./www/**/*.html'], ['html']);
+    gulp.watch(['./www/js/**/*.js'], ['js']);  
+    gulp.watch(['./www/css/**/*.css'], ['css']); 
+    gulp.watch('./www/sass/**/*.scss', ['sass']); 
+})
+
+
+gulp.task('default',['webServe','watch']);
---
+++ www/js/app.js
@@ -0,0 +1,24 @@
+angular.module('MyPc', [])
+    // .controller('MyCtrl', function () {
+
+    // })
+    // .directive('myPosition', function ($uibPosition) {
+    //     return {
+    //         // template: '<ul><li ng-repeat="item in info"><ul><li ng-repeat="(key , value) in item">{{key}} : {{value}}</li></ul></li></ul>',
+    //         template : '<button>Ok</button>',
+    //         scope : {},
+    //         link : function(scope, ele, attrs, ctrl) {
+    //             // // console.log($uibPosition);
+    //             // scope.info = [];
+    //             // scope.info.push($uibPosition.position(ele,true));
+    //             // // scope.info.push($uibPosition.off(ele));
+    //             // console.log(scope.info);
+    //             var s = angular.element('<my-position></my-position>')
+    //         }
+    //     }
+    // })
+    .controller('MyApp',function(){
+        var vm = this;
+        
+        return vm;
+    })
